2f6af507a0f58e976adf649865015151bc0ffa7c
Ashwani Yadav <ashyadavash@gmail.com>
2024-07-09 23:50:40 +0530

    updates

--- lambdas/fyndOMSOrderExport/fyndOMSOrderExportLambda.js
+++ lambdas/fyndOMSOrderExport/fyndOMSOrderExportLambda.js
@@ -1,22 +1,26 @@
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk')
 const util = require('./utility')
+
 exports.handler = async (event) => {
     try {
         console.log("Incoming fyndOMSOrderExportLambda payload", JSON.stringify(event));
-        // Logging Enable
+        
         await Promise.all(event.Records.map(async (record) => {
             const recordBody = JSON.parse(JSON.parse(record.body || '{}'));
             console.log("Record Body", recordBody);
+            
             const getFyndAuthToken = await util.authorisationToken();
-            const orderId= recordBody.payload.order.order_id;
+            const orderId = recordBody.payload.order.order_id;
             const getOrderDetails = await util.getOrderById(orderId, getFyndAuthToken);
-            const transformedOrder = util.orderTransformer(recordBody,getOrderDetails);
+            
+            const transformedOrder = util.orderTransformer(recordBody, getOrderDetails);
             console.log("TRANSFORMED ORDER JSON", JSON.stringify(transformedOrder));
+            
             // XML conversion & Send to S3 bucket
             await util.xmlProcessor(transformedOrder);
-        
-          }));
+        }));
     } catch (e) {
-        console.error('Error crud Lov:', e.toString());
+        console.error('Error in fyndOMSOrderExport Lambda:', e.toString());
+        throw e;  // Throwing error to indicate failure to SQS
     }
 };
--- lambdas/fyndWebhook/fyndWebhookLambda.js
+++ lambdas/fyndWebhook/fyndWebhookLambda.js
@@ -3,14 +3,21 @@ const sqs = new AWS.SQS({ region: process.env.REGION });
 
 exports.handler = async (event) => {
     try {
-        console.log('event.body ====> ', event.body)
-        
-        const params = {
-            MessageBody: JSON.stringify(event.body),
-            QueueUrl: process.env.fyndOrderExportSQS,
-        };
-        await sqs.sendMessage(params).promise();
+        const data = JSON.parse(event.body)
+        console.log('event.body ====> ', data)
 
+        let params = {
+            MessageBody: JSON.stringify(data),
+        };
+
+        if (data.event.name === 'order' && data.event.type === 'placed') {
+            params.QueueUrl = process.env.fyndOrderExportSQS
+        } else {
+            params.QueueUrl = process.env.fyndOrderUpdatesSQS
+        }
+
+        console.log('SQS params = ', params)
+        await sqs.sendMessage(params).promise();
         console.log("Event message sent to SQS (fyndOrderExportSQS) successfully", params);
 
         return {
